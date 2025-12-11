bef9213d11b888c53be120733d896d14bd3c0861
Emmanuel Asamoah <kwakuasamoah1@gmail.com>
2025-12-11 00:47:39 +0000

    updating repo

--- FAQ Accordion/script.js
+++ FAQ Accordion/script.js
@@ -1,10 +1,12 @@
 "use strict";
 const plusBtns = document.querySelectorAll(".plus-btn");
+const btns = document.querySelectorAll(".card__question button");
 const minusBtns = document.querySelectorAll(".minus-btn");
 const questions = document.querySelectorAll(".question");
 
 // Adding handlers to all plusBtns
 plusBtns.forEach((btn) => {
+  const button = btn.closest("button");
   btn.addEventListener("click", function () {
     this.classList.add("hidden");
     this.nextElementSibling.classList.remove("hidden");
@@ -23,6 +25,20 @@ questions.forEach((q) => {
     const btn = this.nextElementSibling;
     const plusBtn = btn.children[0].classList.toggle("hidden");
     const minusBtn = btn.children[1].classList.toggle("hidden");
+  });
+});
+
+// Adding spacebar press to toggle the full text
+btns.forEach((btn) => {
+  btn.addEventListener("keydown", function (e) {
+    if (e.key === " " || e.key === "Space") {
+      e.preventDefault();
+      const plusBtn = btn.children[0].classList.toggle("hidden");
+      const minusBtn = btn.children[1].classList.toggle("hidden");
+      const parent = this.closest(".card__question");
+      const hiddenText = parent.nextElementSibling;
+      hiddenText.classList.toggle("hidden");
+    }
   });
 });
 
